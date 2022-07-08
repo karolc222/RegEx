9e345d64c0967a3b70fa37ff9e16369a2ecae974
Carolina Lupanciuc <103419236+karolc222@users.noreply.github.com>
2022-07-08 15:12:33 +0100

    Add files via upload

---
+++ regex.tuto.py
@@ -0,0 +1,40 @@
+import re 
+
+text_to_search = '''
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+123456789
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped): 
+. ! £ $ % ^ & * + - ? [ ] { } 
+
+coreyms.com 
+
+321-555-4321
+123.555.1234
+123*666*4568
+800-555-1234
+900-555-1234
+
+Mr. Schafer
+Mr Smith 
+Ms Davis 
+Mrs. Robinson 
+Mr. T 
+
+cat 
+mat 
+pat 
+bat
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
+pattern = re.compile(r'start', re.IGNORECASE)
+
+matches = pattern.search(sentence) 
+    
+print(matches)
+ 
